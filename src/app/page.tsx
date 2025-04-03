e416695ef4c24ff605b6b96361345d31c2f45796
Sparsh Aryal <127577030+sparsharyal@users.noreply.github.com>
2025-04-03 20:50:26 +0545

    push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,6 +17,7 @@ export default function Home() {
                         Get started by editing{" "}
                         <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
                             src/app/page.tsx
+                            k
                         </code>
                         .
                     </li>
